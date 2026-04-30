8085f7d5078566bb53c82a75cecf00e1667faa43
Mr-Haywhy <tosinblessing.akinyemi@gmail.com>
2026-04-30 23:00:23 +0100

    computer logic
    Wrote the computer logic and tested it.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello, world!")
+
+// Computer choice logic
+function getComputerChoice() {
+
+    // Generate random number between 0 and 1 using Math.random().
+    let randNum = Math.random();
+
+    // Divide the number range in three equal parts and assign the a return value.
+    if (randNum < 0.33) {
+        return "rock";
+    } else if (randNum < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+// Check out the function
+console.log(getComputerChoice())
+console.log(getComputerChoice())
