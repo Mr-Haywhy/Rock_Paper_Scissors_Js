bf4ca4263a5662a63867f52b51637b8201d4fc93
Mr-Haywhy <tosinblessing.akinyemi@gmail.com>
2026-05-01 01:15:36 +0100

    Added all the logics.
    Completed the computer and the human logics, and also added a single play mode logic.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-console.log("Hello, world!")
+// console.log("Hello, world!")
+
+// Score box
+let humanScore = 0;
+let computerScore = 0;
+
 
 // Computer choice logic
 function getComputerChoice() {
 
     // Generate random number between 0 and 1 using Math.random().
-    let randNum = Math.random();
-    console.log(randNum) 
-    
+    let randNum = Math.random(); 
+
     // Divide the number range in three equal parts and assign the a return value.
     if (randNum < 0.33) {
         return "rock";
@@ -15,12 +19,49 @@ function getComputerChoice() {
     } else {
         return "scissors";
     }
-
-
 }
 
 
-// Check out the function
-console.log(getComputerChoice())
+// test the function
+// console.log(getComputerChoice())
 
 
+function getHumanChoice() {
+    // Get user input using prompt
+    let userInput = prompt("Enter your choice 'rock, paper, scissors': ");
+    // 
+    return userInput.toLowerCase();
+}
+
+// test the functon
+// let userResp = getHumanChoice()
+// console.log(userResp)
+
+
+
+// Game play logic
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    // Check for the winner and update the score box
+    if (humanChoice === computerChoice) {
+        console.log(`Draw: You both chose "${humanChoice}"`);
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper"))
+    {
+        humanScore++    // Add 1 to human score
+        console.log(`You Win! "${ humanChoice }" beats "${computerChoice}"`);
+    } else {
+        computerScore++ // Add 1 to computer score
+        console.log(`You lose! "${computerChoice}" beats "${humanChoice}"`)
+        }
+}
+
+const userResp = getHumanChoice();
+const compResp = getComputerChoice();
+
+playRound(userResp, compResp);
+
+// Print Score Board
+console.log(`Score -> Human: ${humanScore} || Computer: ${computerScore}`);
